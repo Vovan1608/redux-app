cf0d2af835dcacd15b242f7089537e6057f4070f
Volodymyr Zenin <vzeni@softserveinc.com>
2022-06-07 16:07:18 +0300

    add redux-devtools-extension

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers } from 'redux';
 import { cashReducer } from './cashReducer';
 import { customerReducer } from './customerReducer';
+import { composeWithDevTools } from './redux-devtools-extension';
 
 const rootReducer = combineReducers({
     cash: cashReducer, 
